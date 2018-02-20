77c7d9422eae6fa794f28484de99bb0d752e6638
TonyToscana <xxpoblanozxx@gmail.com>
2018-02-20 14:12:19 +0100

    Add basic structure

--- PRACT1/src/sample/Controller.java
+++ PRACT1/src/sample/Controller.java
@@ -1,4 +1,28 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Circle;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class Controller {
+    @FXML
+    private Circle circleBolita;
+    @FXML
+    private GridPane gridBolita;
+
+    int column, fila;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+
+    @FXML
+    private void handleKeyPressedBolita(KeyEvent event) {
+
+    }
 }
