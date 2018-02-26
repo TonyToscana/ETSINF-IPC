d8048a25702154f49bcec581f003ab95c4d45e55
TonyToscana <xxpoblanozxx@gmail.com>
2018-02-26 11:15:31 +0100

    Add Konami Code easter egg

--- PRACT1/src/sample/Controller.java
+++ PRACT1/src/sample/Controller.java
@@ -1,8 +1,10 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 
 import java.net.URL;
@@ -15,10 +17,11 @@ public class Controller {
     private GridPane gridBolita;
 
     int column, row;
-    //private static final String konamiCode = "";
+    private static final String konamiCode = "UPUPDOWNDOWNLEFTRIGHTLEFTRIGHTBA";
+    private final String[] COLORS = {"RED", "BLUE", "GREEN", "BLACK", "GRAY", "PURPLE", "PINK", "GOLD", "SILVER"};
+    private String buffer = "";
 
-
-    public void initialize(URL url, ResourceBundle rb) {
+    public void initialize() {
         column = GridPane.getColumnIndex(circleBolita);
         row = GridPane.getRowIndex(circleBolita);
     }
@@ -27,29 +30,48 @@ public class Controller {
     private void handleKeyPressedBolita(KeyEvent event) {
         switch (event.getCode()) {
             case DOWN:
+                buffer += event.getCode();
+                checkKonamiCode();
                 if (!(row == 4)) {
                     GridPane.setRowIndex(circleBolita, ++row);
                 }
                 break;
             case UP:
+                buffer += event.getCode();
+                checkKonamiCode();
                 if (!(row == 0)) {
                     GridPane.setRowIndex(circleBolita, --row);
                 }
                 break;
             case LEFT:
+                buffer += event.getCode();
+                checkKonamiCode();
                 if (!(column == 0)) {
                     GridPane.setColumnIndex(circleBolita, --column);
                 }
                 break;
             case RIGHT:
+                buffer += event.getCode();
+                checkKonamiCode();
                 if (!(column == 4)) {
                     GridPane.setColumnIndex(circleBolita, ++column);
                 }
                 break;
             default:
-                System.out.println("Bad key pressed");
+                buffer += event.getCode();
+                checkKonamiCode();
+                System.out.println(buffer);
                 break;
 
         }
+    }
+
+    private void checkKonamiCode(){
+        if ((buffer.length() >= konamiCode.length()) &&
+                (buffer.substring(buffer.length()-konamiCode.length()).equals(konamiCode))) {
+            circleBolita.setFill(Paint.valueOf(COLORS[(int) (Math.random() * 9)]));
+            buffer = "";
+        }
+
     }
 }
