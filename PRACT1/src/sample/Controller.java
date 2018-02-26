57a1008035e212b1b4b4291b73e60a70ad1b5add
TonyToscana <xxpoblanozxx@gmail.com>
2018-02-26 10:45:28 +0100

    Add bolita management

--- PRACT1/src/sample/Controller.java
+++ PRACT1/src/sample/Controller.java
@@ -14,16 +14,42 @@ public class Controller {
     @FXML
     private GridPane gridBolita;
 
-    int column, fila;
+    int column, row;
+    //private static final String konamiCode = "";
 
 
     public void initialize(URL url, ResourceBundle rb) {
-
+        column = GridPane.getColumnIndex(circleBolita);
+        row = GridPane.getRowIndex(circleBolita);
     }
 
     @FXML
     private void handleKeyPressedBolita(KeyEvent event) {
-        GridPane.getColumnIndex(circleBolita);
-        GridPane.getRowIndex(circleBolita);
+        switch (event.getCode()) {
+            case DOWN:
+                if (!(row == 4)) {
+                    GridPane.setRowIndex(circleBolita, ++row);
+                }
+                break;
+            case UP:
+                if (!(row == 0)) {
+                    GridPane.setRowIndex(circleBolita, --row);
+                }
+                break;
+            case LEFT:
+                if (!(column == 0)) {
+                    GridPane.setColumnIndex(circleBolita, --column);
+                }
+                break;
+            case RIGHT:
+                if (!(column == 4)) {
+                    GridPane.setColumnIndex(circleBolita, ++column);
+                }
+                break;
+            default:
+                System.out.println("Bad key pressed");
+                break;
+
+        }
     }
 }
